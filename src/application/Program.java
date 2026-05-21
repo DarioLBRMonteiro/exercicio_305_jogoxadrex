8d4121f2c5e0b35f2a6b45f362fee552724d2f99
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-05-20 22:32:13 -0300

    Pequenas melhorias na impressão do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ import chess.ChessMatch;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 //		Position pos = new Position(3, 5);
 //		System.out.println(pos);
 		
